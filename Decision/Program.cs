d1d2ebd7899140b8b0bf900414e198cd606ff853
mbohekale <mbohekale@yahoo.com>
2021-03-17 05:40:42 +0100

    appending files

---
+++ Decision/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bob won the Game");
+            Console.Write("Choose a door: 1 ,2 or 3: ");
+            string userValue = Console.ReadLine();
+            if (userValue == "1")
+            {
+                String message = "You won a new car!";
+                Console.WriteLine(message);
+            }
+            else if (userValue == "2")
+            {
+                String message = "You won a new house!";
+                Console.WriteLine(message);
+            }
+            else if (userValue == "3")
+            {
+                String message = "You won a new wife!";
+                Console.WriteLine(message);
+            }
+            else {
+                String message = "NULL!";
+                Console.WriteLine(message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
