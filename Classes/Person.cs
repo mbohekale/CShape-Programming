6d5c492aebaf9e47254568aabfc6c9b2bd149c8f
mbohekale <mbohekale@yahoo.com>
2021-03-24 07:11:15 +0100

    appending files

---
+++ Classes/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDemo
+{
+    class Person
+    {
+        public int Calculation(int a ,int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Person p = new Person();
+            //p.Calculation(2,3);
+            Console.WriteLine("Solution is  : " + p.Calculation(2,3));
+            Console.ReadLine();
+
+        }
+    }
+}
